ab4e1a28134068df28fa2c46ad99734d004cf771
Frank Dennis Julca Aguilar <faguilar@ime.usp.br>
2018-09-15 09:02:12 -0400

    Update urls in download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -6,7 +6,7 @@ MATH=math768.tar.gz
 cd datasets/flowcharts
 
 echo "Downloading flowcharts dataset..."
-wget http://www.vision.ime.usp.br/~frank.aguilar/datasets/flowcharts768.tar.gz -o $FLOWCHARTS 
+wget http://www.vision.ime.usp.br/~frank.aguilar/graphics/datasets/flowcharts768.tar.gz -o $FLOWCHARTS 
 
 echo "Unzipping..."
 tar -xf $FLOWCHARTS
@@ -14,7 +14,7 @@ tar -xf $FLOWCHARTS
 cd ../../datasets/math
 
 echo "Downloading math dataset..."
-wget http://www.vision.ime.usp.br/~frank.aguilar/datasets/math768.tar.gz -o $MATH
+wget http://www.vision.ime.usp.br/~frank.aguilar/graphics/datasets/math768.tar.gz -o $MATH
 
 echo "Unzipping..."
 tar -xf $MATH
